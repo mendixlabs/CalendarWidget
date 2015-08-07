3fc089f9b21b135e94795408f4e90a266956878f
Robert van 't Hof <robert.van.thof@mendix.com>
2015-08-07 15:00:40 +0200

    Removed trailing comma that could cause issues in IE

--- src/calendar/widget/calendar.js
+++ src/calendar/widget/calendar.js
@@ -4,7 +4,7 @@
 define([
     'dojo/_base/declare', 'mxui/widget/_WidgetBase',
     'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang',
-	'calendar/lib/jquery-2.1.3.min', 'calendar/lib/moment', 'calendar/lib/fullcalendar' , 'calendar/lib/lang-all',
+	'calendar/lib/jquery-2.1.3.min', 'calendar/lib/moment', 'calendar/lib/fullcalendar' , 'calendar/lib/lang-all'
 ], function (declare, _WidgetBase, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, _jQuery, moment, fullCalendar, calendarLang) {
 	'use strict';
 	
