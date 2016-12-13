c2d4136d88357a6ce73e6679ff1461a404eb8bfc
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2016-12-13 19:58:37 +0100

    Update generator

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,4 @@
-// Generated on 2016-11-08 using generator-mendix 2.0.1 :: git+https://github.com/mendix/generator-mendix.git
+// Generated on 2016-12-13 using generator-mendix 2.0.1 :: git+https://github.com/mendix/generator-mendix.git
 /*jshint -W069,-W097*/
 "use strict";
 
